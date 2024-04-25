999a1050c61c2bf872f332543728aa71b42bb5ce
이호진 <lhj@hmit.co.kr>
2024-04-25 18:13:08 +0900

    first init

---
+++ src/main/java/com/example/websocketeasy/WebsocketEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websocketeasy/WebsocketEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
